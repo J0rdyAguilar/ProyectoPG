8a6b0b139935f845da0c94b226043807ca74a86e
Joserpages <jrieckofr@miumg.edu.gt>
2025-12-19 02:20:16 -0600

    prueba

---
+++ Backend/app/Models/evaluaciones.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dependencia extends Model
+{
+    protected $table = 'dependencias';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'ESTADO',
+        'USUARIO_INGRESO',
+        'FECHA_INGRESO',
+        'USUARIO_MODIFICA',
+        
+    ];
+
+    public $timestamps = false;
+}
